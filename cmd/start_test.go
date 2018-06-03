64bf426849a4a1052ebbc5e3648a59bc765dd1a2
Alberto Bregliano <alberto.bregliano@pm.me>
2018-06-04 01:50:17 +0200

    one example only

--- cmd/start_test.go
+++ cmd/start_test.go
@@ -32,7 +32,7 @@ func TestPrimo(t *testing.T) {
 	}
 }
 
-func ExampleIsfobLunedimattina() {
+func Exampleisfob() {
 	date := time.Date(2018, 6, 4, 6, 0, 0, 0, time.Local)
 	ok := isfob(date, 18)
 	fmt.Println(date.Weekday())
@@ -41,51 +41,5 @@ func ExampleIsfobLunedimattina() {
 	//Output:
 	//Monday
 	//6
-	//true
-}
-
-func ExampleIsfobLunedi() {
-	date := time.Date(2018, 6, 4, 8, 0, 0, 0, time.Local)
-	ok := isfob(date, 18)
-	fmt.Println(date.Weekday())
-	fmt.Println(date.Hour())
-	fmt.Println(ok)
-	//Output:
-	//Monday
-	//8
-	//false
-}
-
-func ExampleIsfobLunedifob() {
-	date := time.Date(2018, 6, 4, 18, 0, 0, 0, time.Local)
-	ok := isfob(date, 18)
-	fmt.Println(date.Weekday())
-	fmt.Println(date.Hour())
-	fmt.Println(ok)
-	//Output:
-	//Monday
-	//18
-	//true
-}
-
-func ExampleIsfobDomenica() {
-	date := time.Date(2018, 6, 3, 6, 0, 0, 0, time.Local)
-	ok := isfob(date, 18)
-	fmt.Println(date.Weekday())
-	fmt.Println(date.Hour())
-	fmt.Println(ok)
-	//Output:
-	//Sunday
-	//6
-	//true
-}
-
-func ExampleIsfobSabato() {
-	date := time.Date(2018, 6, 2, 12, 0, 0, 0, time.Local)
-	ok := isfob(date, 18)
-	fmt.Println(date.Weekday())
-	fmt.Println(ok)
-	//Output:
-	//Saturday
 	//true
 }
