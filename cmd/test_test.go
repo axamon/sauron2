35470d50e88b6a0ef26214e7310f395b452d2103
Alberto Bregliano <alberto.bregliano@pm.me>
2018-06-04 08:34:37 +0200

    added test

---
+++ cmd/test_test.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"testing"
+
+	"github.com/axamon/sms"
+)
+
+func TestTest(t *testing.T) {
+	type formati []struct {
+		Numcell string
+		Valido  bool
+	}
+
+	var numeri formati
+	numeri = formati{
+		{Numcell: "3353458144", Valido: false},
+		{Numcell: "+383353458144", Valido: false},
+		{Numcell: "+38335345814", Valido: false},
+		{Numcell: "+393353458144", Valido: true},
+	}
+	for _, cellulare := range numeri {
+		if ok := sms.Verificacellulare(cellulare.Numcell); ok != cellulare.Valido {
+			t.Error("Formato cellulare non valido")
+		}
+	}
+}
