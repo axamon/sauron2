7a241efdf13faa5437552e2f57f090b8531020b0
Alberto Bregliano <alberto.bregliano@pm.me>
2018-06-05 03:49:57 +0200

    alleggerito reperibili

--- cercasid/cercasid_test.go
+++ cercasid/cercasid_test.go
@@ -8,7 +8,10 @@ func TestCercasid(t *testing.T) {
 
 	sid := "CA10c7de2b487b59ceb51917ab81aa2367"
 
-	status, _ := Retrievestatus(sid)
+	status, err := Retrievestatus(sid)
+	if err != nil {
+		t.Errorf("Errore %s", err.Error())
+	}
 	if status != "busy" {
 		t.Skip("Errore nel recupero sid", status)
 	}
