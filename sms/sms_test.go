6e4981e8a9a6506dfb7aed5e4ec11d17ccd0f10e
Alberto Bregliano <alberto.bregliano@pm.me>
2018-06-07 02:48:54 +0200

    test bettered

--- sms/sms_test.go
+++ sms/sms_test.go
@@ -1,6 +1,7 @@
 package sms
 
 import (
+	"os"
 	"testing"
 )
 
@@ -20,6 +21,47 @@ func TestVerificacellulare(t *testing.T) {
 	for _, cellulare := range numeri {
 		if ok := Verificacellulare(cellulare.Numcell); ok != cellulare.Valido {
 			t.Error("Formato cellulare non valido", cellulare.Numcell)
+		}
+	}
+}
+
+func TestRecuperaVariabile(t *testing.T) {
+	type variabile []struct {
+		Nomevar string
+		Value   string
+	}
+	if err := os.Setenv("zoo", "balu"); err != nil {
+		t.Error(err.Error())
+	}
+
+	var element variabile
+	element = variabile{
+		{Nomevar: "zoo", Value: "balu"},
+	}
+	for _, Ele := range element {
+		if result, err := recuperavariabile(Ele.Nomevar); result != Ele.Value {
+			t.Error(err.Error())
+		}
+	}
+
+}
+
+func TestInviasms(t *testing.T) {
+	type formati []struct {
+		Numcell string
+		Valido  bool
+	}
+
+	var numeri formati
+	numeri = formati{
+		{Numcell: "3353458144", Valido: false},
+		{Numcell: "+383353458144", Valido: false},
+		{Numcell: "+38335345814", Valido: false},
+		{Numcell: "+393353458144", Valido: true},
+	}
+	for _, num := range numeri {
+		if result := Inviasms(num.Numcell, "prova"); result != "201 CREATED" {
+			t.Skip("Bisogna settare le variabili")
 		}
 	}
 }
